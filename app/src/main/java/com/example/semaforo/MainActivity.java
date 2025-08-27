10b0111bac80f8b5ea865632451522da8546131d
JairoSolizV <146392601+JairoSolizV@users.noreply.github.com>
2025-08-27 18:05:59 -0400

    Proyecto Semáforo (Fase 3 Completada)

--- app/src/main/java/com/example/semaforo/MainActivity.java
+++ app/src/main/java/com/example/semaforo/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btnActivar;
-    ImageView imgFoco1;
+    ImageView imgFoco1, imgFoco2;
     View mainLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnActivar = findViewById(R.id.btnActivar);
         imgFoco1 = findViewById(R.id.imgFoco1);
+        imgFoco2 = findViewById(R.id.imgFoco2);
         mainLayout = findViewById(R.id.main);
 
 
         btnActivar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Thread thread = new Thread(new Runnable() {
+                Thread threadFocoArriba = new Thread(new Runnable() {
                     int color = 1;
                     @Override
                     public void run() {
@@ -54,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                                             imgFoco1.setImageResource(R.drawable.luz_amarilla);
                                             color = 3;
                                             break;
-                                            case 3:
+                                        case 3:
                                             imgFoco1.setImageResource(R.drawable.luz_verde);
                                             color = 1;
                                             break;
@@ -69,7 +70,40 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-                thread.start();
+                threadFocoArriba.start();
+                Thread threadFocoAbajo = new Thread(new Runnable() {
+                    int color = 1;
+                    @Override
+                    public void run() {
+                        while(true){
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    switch (color) {
+                                        case 1:
+                                            imgFoco2.setImageResource(R.drawable.luz_verde);
+                                            color = 2;
+                                            break;
+                                        case 2:
+                                            imgFoco2.setImageResource(R.drawable.luz_amarilla);
+                                            color = 3;
+                                            break;
+                                        case 3:
+                                            imgFoco2.setImageResource(R.drawable.luz_roja);
+                                            color = 1;
+                                            break;
+                                    }
+                                }
+                            });
+                            try {
+                                Thread.sleep(5000);
+                            } catch (InterruptedException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                });
+                threadFocoAbajo.start();
             }
         });
 
